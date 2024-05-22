e061554d7e28fae139c07e550861ab83bce0a743
Hussain88876 <hassonyemw@gmail.com>
2024-05-22 16:17:23 +1200

    Completed ability to have an editor and save it's content to the backend

--- frontend/src/lib/components/UserInfoForm.svelte
+++ frontend/src/lib/components/UserInfoForm.svelte
@@ -1,11 +1,20 @@
+
+<svelte:head>  <script src="https://cdn.tiny.cloud/1/x0j317jyptd01ki3pnw74apyl45v249opero67lbp6yj5lj7/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script> </svelte:head>
+
+
 <script>
   import { invalidate } from "$app/navigation";
   import { USER_URL } from "$lib/js/api-urls.js";
+  import {onMount} from 'svelte';
   import Component from "./component.svelte";
   export let user;
 
+
+  
+
   let firstName = user.firstName;
   let lastName = user.lastName;
+  let getdisc;
   let desc = user.desc;
   let error = false;
   let success = false;
@@ -25,7 +34,19 @@
     error = !success;
 
     if (success) invalidate(USER_URL);
+
+    
   }
+
+  onMount(() => {
+    tinymce.init({
+        selector: 'textarea'
+    })
+    getdisc = () => {
+        desc = tinymce.get('sup').getContent();
+    }
+  });
+
 </script>
 
 <form on:submit|preventDefault={handleSave}>
@@ -34,12 +55,15 @@
   <label for="lastName">Last name:</label>
   <input type="text" name="lastName" bind:value={lastName} required />
   <textarea id="sup" bind:value={desc} rows="12" required />
-  <button type="submit">Save</button>
+
+  <button type="submit" on:click = {getdisc}>Save</button>
+
   {#if error}<span class="error">Could not save!</span>{/if}
   {#if success}<span class="success">Saved!</span>{/if}
 </form>
 
-<Component/>
+
+
 <style>
   form {
     margin: auto;
