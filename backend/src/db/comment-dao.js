3d8e6182c19c0de3dea110ec5ce01d2500870235
lwan919 <lwan919@aucklanduni.ac.nz>
2024-05-24 17:07:22 +1200

    made comment APIs and daos

--- backend/src/db/comment-dao.js
+++ backend/src/db/comment-dao.js
@@ -1,12 +1,16 @@
-/**
- * This file contains a dummy DAO for the "Messages" database. You should
- * create your own DAOs for your project, and get rid of this one.
- */
-
 import { getDatabase } from "./database.js";
 
-export async function getMessages() {
+export async function postComment(user_id, text, date) {
   const db = await getDatabase();
-  const messages = await db.all("SELECT * FROM Messages");
-  return messages;
+  const postResult = await db.run(`INSERT INTO Comments (user_id, text, date) VALUES (?, ?, ?)`,
+  user_id, text, date);
+
+  // Return true if changes applied, false otherwise
+  return postResult.changes > 0;
+}
+
+export async function getAllComments() {
+  const db = await getDatabase();
+  const comments = await db.all(`SELECT user_id, text, date FROM Comments`);
+  return comments;
 }
