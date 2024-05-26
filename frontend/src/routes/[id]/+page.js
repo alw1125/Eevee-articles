c7d436bc5b47b4cd68c751ab1e5607fdc5cdb81e
EthanFarry <efar683@aucklanduni.ac.nz>
2024-05-27 11:57:42 +1200

    working

---
+++ frontend/src/routes/[id]/+page.js
@@ -0,0 +1,11 @@
+import { PUBLIC_ARTICLES_URL, PUBLIC_USERS_URL } from "$env/static/public";
+
+export async function load({ fetch, params }) {
+  const articleResponse = await fetch(`${PUBLIC_ARTICLES_URL}/${params.id}`);
+  const currentArticle = await articleResponse.json();
+
+  const authorResponse = await fetch(`${PUBLIC_USERS_URL}/${currentArticle.authorId}`);
+  const author = await authorResponse.json();
+
+  return { currentArticle, author };
+}
---
+++ frontend/src/routes/[id]/page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import ArticleView from "$lib/components/ArticleView.svelte";
+  
+    export let data;
+    $: article = data.currentArticle;
+    $: author = data.author;
+  </script>
+  
+  <ArticleView {article} {author} />
