ec9139c421fedaa9a7b8704e2d92604087b398e3
Hussain88876 <hassonyemw@gmail.com>
2024-05-27 12:50:51 +1200

    Case sensitive query search

--- frontend/src/lib/components/SearchBar.svelte
+++ frontend/src/lib/components/SearchBar.svelte
@@ -1,7 +1,17 @@
+<script>
+    let searchQuery="";
+    function caseInsensitiveSearch (){
+    searchQuery= searchQuery.toLowerCase();
+     console.log(searchQuery)
+
+        
+
+    }
+</script>
+    
+
 <form id="form"> 
-    <input type="search" id="query" name="q" placeholder="Search...">
-    <button>Search</button>
+    <input type="search" id="query" name="q" placeholder="Search..." bind:value={searchQuery}>
+    <button on:click={caseInsensitiveSearch}>Search</button>
   </form>
 
-
-  
