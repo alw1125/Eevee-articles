377f682e56ba423a0faa83f96a7746a40bedf80f
Hussain88876 <hassonyemw@gmail.com>
2024-06-04 17:23:15 +1200

    Non-working features gone.

--- frontend/src/routes/[id]/articleEdit/+page.svelte
+++ frontend/src/routes/[id]/articleEdit/+page.svelte
@@ -69,18 +69,13 @@
         width: 600,
         height: 300,
         plugins: [
-          'advlist', 'autolink', 'link', 'image', 'lists', 'charmap', 'preview', 'anchor', 'pagebreak',
+          'advlist', 'lists', 'charmap', 'anchor', 'pagebreak',
           'searchreplace', 'wordcount', 'visualblocks', 'visualchars', 'code', 'fullscreen', 'insertdatetime',
-          'media', 'table', 'emoticons', 'help'
+           'table', 'emoticons'
         ],
         toolbar: 'undo redo | styles | bold italic | alignleft aligncenter alignright alignjustify | ' +
-          'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+          'bullist numlist outdent indent  | ' +
           'forecolor backcolor emoticons | help',
-          
-        menu: {
-          favs: { title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons' }
-        },
-        menubar: 'favs file edit view insert format tools table help',
         content_css: '',
       });
 
