1f4ada91031cbd57220f276dd80fc4daf6047804
EthanFarry <efar683@aucklanduni.ac.nz>
2024-06-04 18:06:17 +1200

    bugfix

--- frontend/src/lib/components/DisplayArticles.svelte
+++ frontend/src/lib/components/DisplayArticles.svelte
@@ -183,6 +183,11 @@
 
   .article-title {
     font-size: 1.7em;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
 
   }
 
